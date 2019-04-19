41909c252c71ccdd85997f942832e694712435c4
Carlos Ferreira <c@rlos.rocks>
2019-04-19 13:22:13 +0100

    PHP extensions required for Demo Symfony project

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -13,8 +13,12 @@ logInfoMsg "Installing NGINX"
 sudo apt-get -qq -y install -o=Dpkg::Use-Pty=0 nginx
 
 # PHP Installation
-logInfoMsg "Installing PHP FPM (Extensions: XML & ZIP)"
-sudo apt-get -qq -y install -o=Dpkg::Use-Pty=0 php7.2-fpm php7.2-xml php7.2-zip
+logInfoMsg "Installing PHP FPM"
+sudo apt-get -qq -y install -o=Dpkg::Use-Pty=0 php7.2-fpm
+
+# PHP Extensions
+logInfoMsg "Installing Required PHP extensions"
+sudo apt-get -qq -y install -o=Dpkg::Use-Pty=0 php7.2-xml php7.2-zip php7.2-curl php7.2-intl php7.2-sqlite3
 
 # Composer Installation
 logInfoMsg "Installing Composer"
